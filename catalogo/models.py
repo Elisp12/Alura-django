dbd74b03f7790f9a46098f1522b7a21dbe771bc2
Elisp12 <eliseuspdam.1999@gmail.com>
2024-07-19 09:18:09 -0300

    models

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Modadalidade(models.Model):
+    index = models.BigAutoField(primary_key=True)
+    nome = models.CharField(max_length=30, null=False)
+
+    def __str__(self):
+        return self.nome
+    
+
+class Peca(models.Model):
+    index = models.BigAutoField(primary_key=True)
+    nome = models.CharField(max_length=30, null=False)
+    descricao = models.TextField()
+    relacao = models.CharField( choices= (('vl','velocidades'), ('un','unica')), max_length=250, null=False)
+    modalidade = models.ForeignKey(Modadalidade, on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return self.nome
