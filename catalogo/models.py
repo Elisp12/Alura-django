d01b7c93342e7066093261c37271d34489eaf299
Elisp12 <eliseuspdam.1999@gmail.com>
2024-07-19 21:56:38 -0300

    update models

--- catalogo/models.py
+++ catalogo/models.py
@@ -12,7 +12,7 @@ class Peca(models.Model):
     index = models.BigAutoField(primary_key=True)
     nome = models.CharField(max_length=30, null=False)
     descricao = models.TextField()
-    relacao = models.CharField( choices= (('vl','velocidades'), ('un','unica')), max_length=250, null=False)
+    relacao = models.CharField( choices= (('velocidade','velocidade'), ('unica','unica')), max_length=250, null=False)
     modalidade = models.ForeignKey(Modadalidade, on_delete = models.CASCADE)
     
     def __str__(self):
