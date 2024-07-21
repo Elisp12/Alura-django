25ff94560b50d83d163b85af22f64d22c8399dd9
Elisp12 <eliseuspdam.1999@gmail.com>
2024-07-21 13:01:33 -0300

    adicionando imagens

--- catalogo/models.py
+++ catalogo/models.py
@@ -13,6 +13,7 @@ class Peca(models.Model):
     nome = models.CharField(max_length=30, null=False)
     descricao = models.TextField()
     relacao = models.CharField( choices= (('velocidade','velocidade'), ('unica','unica')), max_length=250, null=False)
+    imagem = models.ImageField(upload_to= 'media/imagem/%Y/%m/%d/', blank=True)
     modalidade = models.ForeignKey(Modadalidade, on_delete = models.CASCADE)
     
     def __str__(self):
