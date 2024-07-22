c6513e2427a8dbe39825067a9724902e56268c55
Elisp12 <eliseuspdam.1999@gmail.com>
2024-07-22 16:29:35 -0300

    formulário com django

--- usuarios/views/login.py
+++ usuarios/views/login.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from usuarios.forms import Login_formulario
+
 def login(request):
+    form = Login_formulario()
 
     context = {
-
+        'form': form
     }
     return render(request, 'usuarios/login.html', context = context)
 
