deab707d4b9a094ad822f9e120d10ebf5f01fc19
Elisp12 <eliseuspdam.1999@gmail.com>
2024-07-23 15:46:17 -0300

    logout - validaçoes

--- usuarios/views/logout.py
+++ usuarios/views/logout.py
@@ -1,9 +1,14 @@
 from django.shortcuts import redirect
 from django.contrib import auth
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 def logout(request):
-    auth.logout(request)
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado!")
+        return redirect('login')
+    else:
+        auth.logout(request)
+        messages.success(request, 'Logout efetuado com sucesso!')
 
-    messages.success(request, 'Logout efetuado com sucesso!')
     return redirect('lista_peca')
