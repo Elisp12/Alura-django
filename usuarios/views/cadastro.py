667e5e09d6b468035c91d71214836c74c9b58b20
Elisp12 <eliseuspdam.1999@gmail.com>
2024-07-23 11:08:34 -0300

    cadastro - condição para senhas diferentes

--- usuarios/views/cadastro.py
+++ usuarios/views/cadastro.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from usuarios.formulario.cadastro import Cadastro_formulario
 
 def cadastro(request):
     form = Cadastro_formulario()
 
+    if form['senha1'].value() != form['senha2'].value(): #condição para senhas de diferentes
+        return redirect('cadastro') #retorna a página de cadastro
+    
     context = {
         'form': form   
     }
     return render(request, 'usuarios/cadastro.html', context = context)
+
+
+
