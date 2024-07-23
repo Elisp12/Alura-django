7146364c537305d07832c848c477af1a4d76dae9
Elisp12 <eliseuspdam.1999@gmail.com>
2024-07-23 15:53:26 -0300

    logout - coentando código

--- usuarios/views/logout.py
+++ usuarios/views/logout.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 
 def logout(request):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated: # se usuario não estiver logado
         messages.error(request, "Usuário não logado!")
-        return redirect('login')
-    else:
+        return redirect('login') # redireciona para página de login
+    else: # se logado
         auth.logout(request)
         messages.success(request, 'Logout efetuado com sucesso!')
 
