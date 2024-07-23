2e434466e2f1cfc10bc8a589715ca7909f36192a
Elisp12 <eliseuspdam.1999@gmail.com>
2024-07-23 12:07:31 -0300

    cadastro - lógica de cadatro de usuário

--- usuarios/views/cadastro.py
+++ usuarios/views/cadastro.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render, redirect
 
 from usuarios.formulario.cadastro import Cadastro_formulario
+from django.contrib.auth.models import User
 
 def cadastro(request):
     form = Cadastro_formulario()
 
-    if form['senha1'].value() != form['senha2'].value(): #condição para senhas de diferentes
-        return redirect('cadastro') #retorna a página de cadastro
+    if request.method == 'POST': # se o usuário fizer input
+        form = Cadastro_formulario(request.POST) # pega dados do input e passa para formulário
+
+        if form.is_valid():
+            if form['senha1'].value() != form['senha2'].value(): #condição para senhas de diferentes
+                return redirect('cadastro') #retorna a página de cadastro
+
+            nome = form['nome_cadastro'].value() # valores passados por usuários
+            email = form['email'].value()
+            senha = form['senha1'].value()
+
+            if User.objects.filter(username = nome).exists(): # se usuário já exite
+                return redirect('cadastro')
+            
+            usuario = User.objects.create_user( # cria novo usuário no banco de dados
+                username = nome,
+                email = email,
+                password = senha
+            )
+            usuario.save() 
+
+            return redirect('login') # redireciona para página de login
     
     context = {
         'form': form   
