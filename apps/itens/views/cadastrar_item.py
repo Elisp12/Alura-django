e1903d47c1982f5f6cc7f5b097c8d6edfab3b054
Elisp12 <eliseuspdam.1999@gmail.com>
2024-07-24 16:31:56 -0300

    formulário usuário - request

--- apps/itens/views/cadastrar_item.py
+++ apps/itens/views/cadastrar_item.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib import messages
 
 from apps.itens.formulario import Item_formulario
 
 def cadastrar_item(request):
-    form_item = Item_formulario()
+    form = Item_formulario
 
+    if request.method == 'POST':
+        form = Item_formulario(request.POST, request.FILES)
+        
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Novo item Cadastrado!')
+            return redirect('home_usuario')
+        else:
+            messages.error(request, 'error')
+        
+        print(form)
+        
     context = {
-        'form' : form_item
+        'form' : form
     }
 
     return render(request, 'usuarios/novo_item/cadastrar_item.html', context = context)
