0c3147f68197f7acff648a4997a761aa94481271
Elisp12 <eliseuspdam.1999@gmail.com>
2024-07-25 14:41:12 -0300

    item - produto da categoria

--- apps/itens/views/criar_modalidade.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def criar_modalidade(request):
-    return render(request, 'usuarios/novo_item/criar_modalidade.html', context = {})
--- apps/itens/views/peca_modalidade.py
+++ apps/itens/views/peca_modalidade.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
 from apps.catalogo.models import Peca
+from apps.itens.formulario import Item_formulario
 
 def peca_da_modalidade(request, index):
     item = Peca.objects.filter(modalidade = index).all()
-    print(item)
+    
     context = {
         'peca_da_modalidade': item
     }
-
     return render(request, 'peca/peca_da_modalidade.html', context = context)
