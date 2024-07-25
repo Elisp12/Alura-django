cfaa65fbfeb1e17e81689746433cfb54ad10f114
Elisp12 <eliseuspdam.1999@gmail.com>
2024-07-25 11:04:11 -0300

    logout - atualizado

--- apps/catalogo/views/lista_peca.py
+++ apps/catalogo/views/lista_peca.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from apps.catalogo.models import Modadalidade, Peca
-
+from django.contrib import messages
 from django.contrib.auth.models import User
 
 
 def lista_peca(request):
     peca = Peca.objects.all()
+    
+    
 
     context = {
         'lista_peca': peca
--- apps/usuarios/views/logout.py
+++ apps/usuarios/views/logout.py
@@ -4,11 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 
 def logout(request):
-    if not request.user.is_authenticated: # se usuario não estiver logado
-        messages.error(request, "Usuário não logado!")
-        return redirect('login') # redireciona para página de login
-    else: # se logado
-        auth.logout(request)
-        messages.success(request, 'Logout efetuado com sucesso!')
+    auth.logout(request)
+    messages.success(request, 'Logout efetuado com sucesso!')
 
     return redirect('lista_peca')
