c301a39edcf121c18e5febfb7e1dd044cedd1219
Elisp12 <eliseuspdam.1999@gmail.com>
2024-08-04 13:14:40 -0300

    Formuário: request, trazendo dados para views

--- apps/itens/views/cadastrar_item.py
+++ apps/itens/views/cadastrar_item.py
@@ -1,26 +1,29 @@
 from django.shortcuts import render, redirect
-
 from django.contrib import messages
 
-from apps.itens.formulario import Item_formulario
+from apps.catalogo.models import Modalidade, Peca
 
 def cadastrar_item(request):
-    form = Item_formulario
+
+    nome = request.POST.get('nome')
+    descricao = request.POST.get('descricao')
+    relacao = request.POST.get('relacao')
+    imagem = request.POST.get('imagem')
+    modalidade = request.POST.get('modalidade')
 
     if request.method == 'POST':
-        form = Item_formulario(request.POST, request.FILES)
+        Peca.objects.update_or_create(nome = nome, descricao = descricao, relacao = relacao, imagem = imagem, modalidade = Modalidade.objects.get(index = modalidade))
+
+        messages.success(request, 'Item criado com sucesso!')
         
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Novo item Cadastrado!')
-            return redirect('home_usuario')
-        else:
-            messages.error(request, 'error')
-        
-        print(form)
-        
+        return redirect('home_usuario')
+    
+    lista_modalidade = Modalidade.objects.all()
+    lista_itens = Peca.objects.all()
+
     context = {
-        'form' : form
+        'lista_item': lista_itens,
+        'lista_modalidade': lista_modalidade
     }
 
     return render(request, 'usuarios/novo_item/cadastrar_item.html', context = context)
